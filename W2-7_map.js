0da6706026713680eee09cb8ba810179af50c68c
Faiem-qy <f.quanyoung@gmail.com>
2023-04-01 22:48:01 -0400

    lotide

--- W2-7_map.js
+++ W2-7_map.js
@@ -1,3 +1,26 @@
+
+let eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i])
+      return false;
+  }
+  return true;
+};
+
+
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed:${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  };
+};
+
 const words = ["ground", "control", "to", "major", "tom"];
 const results1 = map(words, word => word[0]);
 console.log(results1);
@@ -6,9 +29,8 @@ console.log(results1);
 const map = function (array, callback) {
   const results = [];
   for (let item of array) {
-    console.log('array:', array);
-    console.log('callback', callback);
-
+    results.push(callback(item));
   }
+
   return results;
 };
