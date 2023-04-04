624594d24a70028af997d6a6ac49dd840be0d7cb
Faiem-qy <f.quanyoung@gmail.com>
2023-04-03 23:17:12 -0400

    letters

--- w2-2_countLetters.js
+++ w2-2_countLetters.js
@@ -1,25 +1,25 @@
 
-// let eqArrays = function (arr1, arr2) {
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   }
+let eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
 
-//   for (let i = 0; i < arr1.length; i++) {
-//     if (arr1[i] !== arr2[i])
-//       return false;
-//   }
-//   return true;
-// };
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i])
+      return false;
+  }
+  return true;
+};
 
 
 
-// const assertEqual = function (actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion passed:${actual} === ${expected}`);
-//   } else {
-//     console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-//   };
-// };
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed:${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  };
+};
 
 
 // TEST CODE
