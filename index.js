92393543793631c664077192d81b89d76b9ac4dd
Faiem-qy <f.quanyoung@gmail.com>
2023-04-13 22:48:45 -0400

    index

---
+++ index.js
@@ -0,0 +1,10 @@
+// index.js
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./5.middle');
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle
+};
